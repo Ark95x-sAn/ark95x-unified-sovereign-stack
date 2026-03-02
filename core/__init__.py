c2a5b0ed12ddbb0361bcdf62a936d86e03730590
Ark95x | Sovereign Architect <bnordskog45@gmail.com>
2026-03-02 02:46:07 -0600

    Add core module - FROM: ark95x-omnikernel-orchestrator

---
+++ core/__init__.py
@@ -0,0 +1,4 @@
+"""Core Module - FROM: ark95x-omnikernel-orchestrator
+Adaptive agent scheduling, self-healing, DAG pipelines, telemetry.
+HLM-9, CrewAI Agents, Clone Manager, Gatekeeper, Decision Router.
+"""
